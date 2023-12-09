784a4585cd25fb9e883039aa817b1fa5e3ed1058
goekmavi <goekmavidev@gmail.com>
2023-12-09 01:22:23 +0100

    Add solution for day 8 part 1

--- day-8/index.js
+++ day-8/index.js
@@ -1,8 +1,65 @@
 const fs = require('fs');
 
+const getPos = (valuesArr, val) => {
+   return valuesArr.findIndex(item => {
+        let currentVal = item.split('=')[0].trim();
+
+        if (currentVal === val) {
+            return true;
+        }
+    });
+}
+
+const getValues = (valuesArr, pos) => {
+    let [currentValue, nextValues] = valuesArr[pos].split('=').map(val => {
+        let tempValue = val.trim();
+
+        if (tempValue[0] === '(' && tempValue[tempValue.length - 1] === ')') {
+            tempValue = tempValue.slice(1, -1).split(',').map(val => val.trim());
+        }
+
+        return tempValue;
+    });
+
+    let [leftValue, rightValue] = nextValues;
+
+    return [currentValue, leftValue, rightValue];
+}
+
 const solveSilver = (instructions, values) => {
-    console.log(instructions);
-    console.log(values);
+    const instructionsChars = instructions.split('');
+    const valuesArr = values.split('\n');
+
+    let reachedEnd = false;
+    let index = 0;
+    let steps = 0;
+    let pos = getPos(valuesArr, 'AAA');
+
+    while (!reachedEnd) {
+        let [currentValue, leftValue, rightValue] = getValues(valuesArr, pos);
+        let currentInstruction = instructionsChars[index];
+
+        if (currentValue === 'ZZZ') {
+            reachedEnd = true;
+            pos = 0;
+        } else {
+            steps++;
+        }
+
+        if (currentInstruction === 'L') {
+            pos = getPos(valuesArr, leftValue);
+        } else if (currentInstruction === 'R') {
+            pos = getPos(valuesArr, rightValue);
+        }
+
+        if (index === instructionsChars.length - 1) {
+            index = 0;
+        } else {
+            index++;
+        }
+    }
+
+    return steps;
 }
 
 try {
